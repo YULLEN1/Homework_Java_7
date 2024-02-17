823756c1e3bba5fda4b79c3f53a292222b6aa5d2
Yulia Lener <yulias.docs@gmail.com>
2024-02-17 21:30:17 +0700

    main

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -18,15 +18,15 @@ public class StatisticsServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
-@Test
-void findMaxTestCoverage() {
-    StatisticsService service = new StatisticsService();
+    @Test
+    void findMaxTestCoverage() {
+        StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {3, 5, 8, 4, 5, 3, 8, 6, 11, 11, 10};
-    long expected = 11;
+        long[] incomesInBillions = {3, 5, 8, 4, 5, 3, 8, 6, 11, 11, 10};
+        long expected = 11;
 
-    long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
-    Assertions.assertEquals(expected, actual);
-}
+        Assertions.assertEquals(expected, actual);
+    }
 }
